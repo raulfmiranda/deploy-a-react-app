76a3ee1fbe564b1fba10151785645817558b9f2d
RaulMirandaOSF <raul.miranda@osf-commerce.com>
2019-05-21 15:36:19 -0300

    Updates

---
+++ precache-manifest.19be26d9e6f222a854eb2d69195a02a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c737611deab592b09c35",
+    "url": "/deploy-a-react-app/static/js/runtime~main.c737611d.js"
+  },
+  {
+    "revision": "9396ae3af134a89adeab",
+    "url": "/deploy-a-react-app/static/js/main.9396ae3a.chunk.js"
+  },
+  {
+    "revision": "f5ae2494a8dbd9f95f4b",
+    "url": "/deploy-a-react-app/static/js/1.f5ae2494.chunk.js"
+  },
+  {
+    "revision": "9396ae3af134a89adeab",
+    "url": "/deploy-a-react-app/static/css/main.2f3a6014.chunk.css"
+  },
+  {
+    "revision": "148b70f75dd0cd507884fba78de4dd6c",
+    "url": "/deploy-a-react-app/index.html"
+  }
+];
