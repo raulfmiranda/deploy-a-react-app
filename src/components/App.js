9ca4ec5282b4904afdcd612090ce99c582b76c19
RaulMirandaOSF <raul.miranda@osf-commerce.com>
2019-05-21 15:45:16 -0300

    Config to deploy to github pages

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ import NotFound from './NotFound';
 import Featured from './Featured';
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename="/deploy-a-react-app">
     <div className="container">
       <Header />
       
